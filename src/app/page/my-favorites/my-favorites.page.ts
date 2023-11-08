f8e24905cbe24240d3a3f0de254ceed8ca01be8a
Inma Caleron <inmacale@gmail.com>
2023-11-08 12:00:01 +0100

    errores coregidos de eliminacion

--- src/app/page/my-favorites/my-favorites.page.ts
+++ src/app/page/my-favorites/my-favorites.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-my-favorites',
@@ -7,9 +8,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MyFavoritesPage implements OnInit {
 
-  constructor() { }
+  constructor(private alertController: AlertController) { }
 
   ngOnInit() {
   }
 
+  async deleteItem(event: any, x: any) {
+    console.log(event)
+    const slidingItem = event.target.closest('ion-item-sliding');
+    slidingItem.close();
+
+    const alert = await this.alertController.create({
+      header: 'Confirmar borrado',
+      message: `¿Estás seguro de que quieres borrar ${x}?`,
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancel'
+        }, {
+          text: 'Borrar',
+          handler: () => {
+            // Aquí va la lógica para borrar el ítem
+            console.log('Ítem borrado:', x);
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+  }
 }
