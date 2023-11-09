add04c02056881d4ed827018f4e06a3af8d007cc
Inma Caleron <inmacale@gmail.com>
2023-11-09 09:29:47 +0100

    subida del metodo toggleFavorite de characters

--- src/app/page/characters/characters.page.ts
+++ src/app/page/characters/characters.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-characters',
@@ -6,10 +7,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./characters.page.scss'],
 })
 export class CharactersPage implements OnInit {
+  isFavorite: boolean = false;
 
-  constructor() { }
+  constructor(private alertController: AlertController) { }
 
   ngOnInit() {
+  }
+
+
+  async toggleFavorite() {
+    if (this.isFavorite) {
+      const alert = await this.alertController.create({
+        header: 'Quitar favorito',
+        message: `¿Estás seguro de que quieres quitar como favorito?`,
+        buttons: [
+          {
+            text: 'Cancelar',
+            role: 'cancel'
+          }, {
+            text: 'Eliminar',
+            handler: () => {
+              this.isFavorite = !this.isFavorite;
+              // Aquí va la lógica para borrar el ítem
+              console.log('Ítem borrado:');
+            }
+          }
+        ]
+      });
+
+      await alert.present();
+
+    } else {
+      this.isFavorite = !this.isFavorite;
+    }
+
   }
 
 }
--- src/app/page/my-favorites/my-favorites.page.ts
+++ src/app/page/my-favorites/my-favorites.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AlertController } from '@ionic/angular';
 
+
 @Component({
   selector: 'app-my-favorites',
   templateUrl: './my-favorites.page.html',
